aead865906ba36053353d57688f7d0b313fddc04
Noah Gerard <97645851+noahgerard@users.noreply.github.com>
2026-03-18 23:47:43 -0400

    it works dangit 🤦♂️

--- mysh.c
+++ mysh.c
@@ -73,7 +73,6 @@ out:
   ensure(lookahead, END_OF_LINE);
 
   // TODO: Be sure to remove any debugging output to stdout before submitting
-  printf("PRINTING COMMANDS!\n");
   print_commands();
 }
 
@@ -88,6 +87,26 @@ void consume() {
 int main(int argc, char **argv) {
   parse();
   run_commands();
+  printf("commands all run, next\n");
+}
+
+void close_out_end(int pipefd[2]) {
+  if (-1 == close(pipefd[0])) {
+    perror("close");
+    exit(EXIT_FAILURE);
+  }
+}
+void close_in_end(int pipefd[2]) {
+  if (-1 == close(pipefd[1])) {
+    perror("close");
+    exit(EXIT_FAILURE);
+  }
+}
+void wait_till_children_die() {
+  do {
+    while (wait(NULL) > 0)
+      ;
+  } while (errno != ECHILD);
 }
 
 void run_commands() {
@@ -104,6 +123,7 @@ void run_commands() {
       break;
     case 0: // child
 
+      // Input redirection
       if (NULL != command->in) {
         int infd = open(command->in, O_RDONLY);
         if (-1 == infd) {
@@ -120,6 +140,7 @@ void run_commands() {
         }
       }
 
+      // Output redirection
       if (NULL != command->out) {
         int outfd =
             open(command->out, O_RDWR | O_TRUNC | O_CREAT, S_IRUSR | S_IWUSR);
@@ -148,9 +169,81 @@ void run_commands() {
     }
 
     // wait for children to exit
-    do {
-      while (wait(NULL) > 0)
-        ;
-    } while (errno != ECHILD);
+    wait_till_children_die();
+  } else if (NULL != first_command) {
+    command_t *command = first_command;
+    command_t *prev_command = NULL;
+    int pipefd[2];
+    int prev_pipe_out = -1;
+
+    while (NULL != command) {
+      // Open pipe and store fd's in pipefd
+      if (command->pipe_next) {
+        if (-1 == pipe(pipefd)) {
+          perror("pipe");
+          exit(EXIT_FAILURE);
+        }
+      }
+
+      pid_t pid;
+      switch (pid = fork()) {
+      case -1:
+        perror("fork");
+        exit(EXIT_FAILURE);
+        break;
+      case 0: // child
+        if (prev_pipe_out != -1) {
+          if (-1 == dup2(prev_pipe_out, STDIN_FILENO)) {
+            perror("dup2");
+            exit(EXIT_FAILURE);
+          }
+          if (-1 == close(prev_pipe_out)) {
+            perror("close");
+            exit(EXIT_FAILURE);
+          }
+        }
+
+        if (command->pipe_next) {
+          if (-1 == close(pipefd[0])) {
+            perror("close");
+            exit(EXIT_FAILURE);
+          }
+          if (-1 == dup2(pipefd[1], STDOUT_FILENO)) {
+            perror("dup2");
+            exit(EXIT_FAILURE);
+          }
+          if (-1 == close(pipefd[1])) {
+            perror("close");
+            exit(EXIT_FAILURE);
+          }
+        }
+
+        execvp(command->argv[0], command->argv);
+        perror("execvp");
+        _exit(EXIT_FAILURE);
+        break;
+      default: // parent
+        if (prev_pipe_out != -1) {
+          close(prev_pipe_out);
+          prev_pipe_out = -1; // Reset it
+        }
+
+        // 2. If we just created a NEW pipe for the current child to write into:
+        if (command->pipe_next) {
+          // We MUST close the write end in the parent.
+          // Only the child should be writing to this.
+          close(pipefd[1]);
+
+          // Now, save the read end as the baton for the NEXT loop.
+          prev_pipe_out = pipefd[0];
+        }
+
+        command = command->next;
+        break;
+      }
+    }
+
+    // Close pipe fds
+    wait_till_children_die();
   }
 }
